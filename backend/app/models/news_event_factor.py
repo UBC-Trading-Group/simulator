7a7872f96ce7a106443f8b89f464534c10a892e3
erik-wang-lancedb <erik@lancedb.com>
2025-11-11 15:20:49 -0800

    Make migration and table population script

--- backend/app/models/news_event_factor.py
+++ backend/app/models/news_event_factor.py
@@ -2,13 +2,11 @@
 News Event Factor model (junction table)
 """
 
-from uuid import UUID
-
 from sqlmodel import Field, ForeignKey, SQLModel
 
 
 class NewsEventFactor(SQLModel, table=True):
     __tablename__ = "news_event_factors"
 
-    news_event_id: UUID = Field(foreign_key="news_events.id", primary_key=True)
-    factor_id: UUID = Field(foreign_key="macro_factors.id", primary_key=True)
+    news_event_id: int = Field(foreign_key="news_events.id", primary_key=True)
+    factor_id: str = Field(foreign_key="macro_factors.id", primary_key=True)
